39ab1ee4b7ca632efcd834b4a59c9a8b322b6bdf
amitpoitrix <amit.poitrix@gmail.com>
2025-06-01 09:39:45 +0530

    fix changes in TransactionController

--- src/main/java/com/example/basic/demo/controller/TransactionController.java
+++ src/main/java/com/example/basic/demo/controller/TransactionController.java
@@ -16,35 +16,50 @@ public class TransactionController {
 
     // Create the account
     @PostMapping("create")
-    public ResponseEntity<TransactionResponse> createAccount(@RequestParam String accountId, @RequestParam BigDecimal balance) {
+    public ResponseEntity<TransactionResponse> createAccount(
+            @RequestParam String accountId,
+            @RequestParam BigDecimal balance
+    ) {
         TransactionResponse transactionResponse = transactionService.createAccount(accountId, balance);
         return ResponseEntity.ok(transactionResponse);
     }
 
     // deposit amount in accountId
     @PostMapping("/{id}/deposit")
-    public ResponseEntity<TransactionResponse> deposit(@PathVariable("id") String accountId, @RequestParam BigDecimal amount) throws IllegalAccessException {
+    public ResponseEntity<TransactionResponse> deposit(
+            @PathVariable("id") String accountId,
+            @RequestParam BigDecimal amount
+    ) throws IllegalAccessException {
         TransactionResponse deposit = transactionService.deposit(accountId, amount);
         return ResponseEntity.ok(deposit);
     }
 
     // withdraw the amount from accountId
     @PostMapping("/{id}/withdraw")
-    public ResponseEntity<TransactionResponse> withdraw(@PathVariable("id") String accountId, @RequestParam BigDecimal amount) {
+    public ResponseEntity<TransactionResponse> withdraw(
+            @PathVariable("id") String accountId,
+            @RequestParam BigDecimal amount
+    ) {
         TransactionResponse transactionResponse = transactionService.withdraw(accountId, amount);
         return ResponseEntity.ok(transactionResponse);
     }
 
     // transfer the amount from Account A to B
     @PostMapping("/transfer")
-    public ResponseEntity<TransactionResponse> transfer(@RequestParam String fromId, @RequestParam String toId, @RequestParam BigDecimal amount) {
+    public ResponseEntity<TransactionResponse> transfer(
+            @RequestParam String fromId,
+            @RequestParam String toId,
+            @RequestParam BigDecimal amount
+    ) {
         TransactionResponse transactionResponse = transactionService.transfer(fromId, toId, amount);
         return ResponseEntity.ok(transactionResponse);
     }
 
     // Get balance from an account
     @GetMapping("/{id}/balance")
-    public ResponseEntity<TransactionResponse> getBalance(@PathVariable("id") String accountId) {
+    public ResponseEntity<TransactionResponse> getBalance(
+            @PathVariable("id") String accountId
+    ) {
         TransactionResponse transactionResponse = transactionService.getBalance(accountId);
         return ResponseEntity.ok(transactionResponse);
     }
